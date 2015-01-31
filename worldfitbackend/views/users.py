8042053e2549f0d8a357e28f5e99084932f579d6
ouvigna <cesarpereziglesias@gmail.com>
2015-01-31 18:51:54 +0100

    Fix return of last 7 activities

--- worldfitbackend/views/users.py
+++ worldfitbackend/views/users.py
@@ -47,7 +47,7 @@ class Users:
         if user is None:
             return HTTPNotFound()
 
-        return sorted([activity.to_dict() for activity in user.activities], key=lambda activity: activity['date'])[-7:0]
+        return sorted([activity.to_dict() for activity in user.activities], key=lambda activity: activity['date'])[-7:]
 
 
     @view_config(route_name='user_activities_register', renderer='json')
