84f0e3caf5b2e3ceb85428598cf1703034400518
ouvigna <cesarpereziglesias@gmail.com>
2015-01-31 18:23:13 +0100

    Change insert activities in helper script

--- bin/insert_activities_steps.sh
+++ bin/insert_activities_steps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -H "Content-Type: application/json" -d '[{"value": 100, "date": "2015/01/01"}, {"value": 151, "date": "2015/01/02"}]' http://localhost:6543/users/$1/activities/steps
+curl -H "Content-Type: application/json" -d '[{"value": 100, "date": "2015/01/01"}, {"value": 151, "date": "2015/01/02"}]' http://localhost:6543/users/$1/activities
